e59cd3098b6c47c25c2da764322881769c1ae13a
brianontiri <foughtsentertainment@gmail.com>
2017-07-07 14:48:06 +0300

    added an id

--- lib/defination.rb
+++ lib/defination.rb
@@ -1,4 +1,43 @@
 class Defination
-  @@description = []
+  @@definatios = []
 
-  define_method(:initialize) do ||
+  define_method(:initialize) do |defination|
+    @defination = defination
+    @id = @@definations.length().+(1)
+    @words = []
+  end
+
+  define_method(:defination) do
+    @defination
+  end
+
+  define_method(:id) do
+    @id
+  end
+
+  define_method(:words) do
+    @words
+
+  define_singleton_method(:all) do
+    @@definations
+  end
+
+  define_singleton_method(:save) do
+    @@definations.push(self)
+  end
+
+  define_singleton_method(:clear) do
+    @@definations = []
+  end
+
+  define_singleton_method(:find) do
+    found_defination = nil
+    @@definations.each() do |defination|
+      if defination.id().eql?(id)
+        found_defination = defination
+      end
+    end
+    found_defination
+  end
+end
+  
---
+++ spec/definationspec.rb
@@ -0,0 +1,61 @@
+require('rspec')
+require('defination')#requires from the defination.rb file
+
+describe('Defination') do
+  before() do
+    Defination.clear()
+  end
+
+  describe('#defination')
+  it("returns the defination ") do
+    test_defination = Defination.new("capital city of kenya")
+    expect(test_defination.defination()).to(eq("capital city of kenya"))
+  end
+end
+
+describe('#id') do
+  it("returns the id ") do \
+    test_defination = Defination.new("capital city")
+    expect(test_defination.id()).to(eq(1))
+  end
+end
+
+describe('#words') do
+  it("initially returns an empty array of words ") do
+    test_defination = Defination.new("capital city")
+    expect(test_defination.words()).to(eq([]))
+  end
+end
+
+ describe("#save") do
+   it("adds a defination to the array of saved definations") do
+     test_defination = Defination.new("an animal")
+     test_defination.save()
+     expect(Defination.all()).to(eq([test_defination]))
+   end
+ end
+
+ describe(".all") do
+   it("its empty at first") do
+     expect(Defination.all()).to(eq([]))
+   end
+ end
+
+ describe(".clear") do
+   it("empties all saved definations") do
+     Defination.new("a cat family").save()
+     Defination.clear()
+     expect(Defination.all()).to(eq([]))
+   end
+ end
+
+ describe(".find") do
+  it("returns a defination by its id number") do
+    test_defination = Defination.new("kenyan anthem")
+    test_defination.save()
+    test_defination2 = Defination.new("kenyan politician")
+    test_defination2.save()
+    expect(Defination.find(test_defination.id())).to(eq(test_defination))
+    end
+    end
+ end
